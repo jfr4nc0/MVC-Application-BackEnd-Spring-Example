fb7f5501f22ee632a50f56f6d79e70f9bb03152f
Joan <jfcpersonal99@gmail.com>
2022-12-11 16:11:44 -0300

    backend OK

--- itr/src/main/java/com/itr/app/ItrApplication.java
+++ itr/src/main/java/com/itr/app/ItrApplication.java
@@ -1,11 +1,7 @@
 package com.itr.app;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.itr.app.repositories.JugadorRepository;
 
 @SpringBootApplication
 public class ItrApplication {
---
+++ itr/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+/* 
+    La base de datos utilizada en MariaDB, las credenciales de la base se modifican en el archivo "application.properties" con ruta en "src/main/resources".
+    Los campos de credencial son:
+        spring.datasource.username=
+        spring.datasource.password=
+    Se debera ingresar las credenciales correspondientes.
+    La tabla "jugador" se crea de forma automatica.
+*/
+
+CREATE DATABASE `challenge` 
+/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */
+
